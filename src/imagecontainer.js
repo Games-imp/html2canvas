01e4df01c28527146785635799e052604cb6aaa4
imp <imp@fanruan.com>
2018-07-19 16:15:28 +0800

    add bug annotation

--- src/imagecontainer.js
+++ src/imagecontainer.js
@@ -12,6 +12,7 @@ function ImageContainer(src, cors) {
         //BI-12645 chrome会缓存图片 canvas画缓存的图片会跨域
         var mark = src.indexOf && src.indexOf("?") > -1 ? "&" : "?";
         // self.image.src = src + mark + 'id=' + Math.random();
+        // BI-26932 5.0地图图片截图
         self.image.src = src;
         if (self.image.complete === true) {
             resolve(self.image);
